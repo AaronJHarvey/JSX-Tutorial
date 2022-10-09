0c9970148fe05b52ce1f9b4a320d833ba07c3bcd
Aaron <Harvey.AaronJ@gmail.com>
2022-10-08 22:27:33 -0400

    Adds basic JSX to component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import './App.css';
 import React from 'react';
 
 function App () {
-	return null;
+	return (
+		// In order to return multiple elements in JSX we must nest them within one singular element 
+		<>
+			<h1>Hello, World</h1>
+			<p>I am writing JSX</p>
+		</>
+	)
 }
 
 export default App;
