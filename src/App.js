a565126d002a878f33e155469f2bffa0ae9a893a
Aaron <Harvey.AaronJ@gmail.com>
2022-10-09 22:43:03 -0400

    Uses JSX variables to set id of h1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import React from 'react';
 
 function App () {
+	const greeting = 'greeting';
 	return (
 		// In order to return multiple elements in JSX we must nest them within one singular element
 		<div className='container'>
-			<h1>Hello, World</h1>
+			<h1 id={greeting}>Hello, World</h1>
 			<p>I am writing JSX</p>
 		</div>
 	);
