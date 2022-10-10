9ba6bb3b2673b234b6af45b5c2e1fb33cd56f2c7
Aaron <Harvey.AaronJ@gmail.com>
2022-10-09 22:40:22 -0400

    Adds className and div to empty <> to style

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React from 'react';
 
 function App () {
 	return (
-		// In order to return multiple elements in JSX we must nest them within one singular element 
-		<>
+		// In order to return multiple elements in JSX we must nest them within one singular element
+		<div className='container'>
 			<h1>Hello, World</h1>
 			<p>I am writing JSX</p>
-		</>
-	)
+		</div>
+	);
 }
 
 export default App;
